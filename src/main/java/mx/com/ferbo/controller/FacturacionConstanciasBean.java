1be5d1c28a33f3b31bb2e80a001aa08c61ad3300
Giovanny2999 <monti290399@hotmail.com>
2023-03-30 11:16:40 -0600

    datos emisor

--- src/main/java/mx/com/ferbo/controller/FacturacionConstanciasBean.java
+++ src/main/java/mx/com/ferbo/controller/FacturacionConstanciasBean.java
@@ -3,13 +3,20 @@ package mx.com.ferbo.controller;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
 import mx.com.ferbo.dao.ClienteDAO;
+import mx.com.ferbo.dao.ClienteDomiciliosDAO;
+import mx.com.ferbo.dao.PlantaDAO;
 import mx.com.ferbo.model.Cliente;
+import mx.com.ferbo.model.ClienteDomicilios;
+import mx.com.ferbo.model.Domicilios;
+import mx.com.ferbo.model.Planta;
+
 
 @Named
 @ViewScoped
@@ -18,18 +25,29 @@ public class FacturacionConstanciasBean implements Serializable{
 	private static final long serialVersionUID = -1785488265380235016L;
 	
 	private Cliente clienteSelect;
+	private Domicilios domicilioSelect;
+	private Planta plantaSelect;
 	
 	private ClienteDAO clienteDAO;
+	private ClienteDomiciliosDAO clienteDomicilioDAO;
+	private PlantaDAO plantaDAO;
 	
 	private List<Cliente> listaCliente;
+	private List<ClienteDomicilios> listaClienteDom;//recupera datos de la tabla cliente-domicilio
+	private List<ClienteDomicilios> listaClienteDomicilio;
+	private List<Planta> listaPlanta;
 	
 	
 	public FacturacionConstanciasBean() {
 		
 		clienteDAO = new ClienteDAO();
+		clienteDomicilioDAO = new ClienteDomiciliosDAO();
+		plantaDAO = new PlantaDAO();
 		
 		listaCliente = new ArrayList<>();
-		
+		listaClienteDom = new ArrayList<>();
+		listaClienteDomicilio = new ArrayList<>();
+		listaPlanta = new ArrayList<>();
 		
 	}
 	
@@ -37,6 +55,9 @@ public class FacturacionConstanciasBean implements Serializable{
 	public void init() {
 		
 		listaCliente = clienteDAO.buscarTodos();
+		listaClienteDom = clienteDomicilioDAO.buscarTodos();
+		listaPlanta = plantaDAO.findall();
+		
 	}
 
 	public Cliente getClienteSelect() {
@@ -54,11 +75,55 @@ public class FacturacionConstanciasBean implements Serializable{
 	public void setListaCliente(List<Cliente> listaCliente) {
 		this.listaCliente = listaCliente;
 	}
+
+	public Domicilios getDomicilioSelect() {
+		return domicilioSelect;
+	}
+
+	public void setDomicilioSelect(Domicilios domicilioSelect) {
+		this.domicilioSelect = domicilioSelect;
+	}
+
+	public List<ClienteDomicilios> getListaClienteDomicilio() {
+		return listaClienteDomicilio;
+	}
+
+	public void setListaClienteDomicilio(List<ClienteDomicilios> listaClienteDomicilio) {
+		this.listaClienteDomicilio = listaClienteDomicilio;
+	}
+
+	public Planta getPlantaSelect() {
+		return plantaSelect;
+	}
+
+	public void setPlantaSelect(Planta plantaSelect) {
+		this.plantaSelect = plantaSelect;
+	}
 	
+	public List<Planta> getListaPlanta() {
+		return listaPlanta;
+	}
+
+	public void setListaPlanta(List<Planta> listaPlanta) {
+		this.listaPlanta = listaPlanta;
+	}
 
 	public void domicilioCliente() {
 		
+		listaClienteDomicilio.clear();
+		listaClienteDomicilio = listaClienteDom.stream()
+								.filter(cd -> clienteSelect != null
+								?(cd.getCteCve().getCteCve().intValue()==clienteSelect.getCteCve().intValue())
+								:false)
+								.collect(Collectors.toList());
+		if(listaClienteDomicilio.size()>0) {
+			domicilioSelect = listaClienteDomicilio.get(0).getDomicilios();
+			
+		}
+		
+		
 	}
+	
 	
 	
 
