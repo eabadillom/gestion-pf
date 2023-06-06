c8493cbb3ed16db471997fc611dc846986300310
judit_gs <judit.saldivar@hotmail.com>
2023-06-06 18:00:24 -0500

    Se anexa metodo para la exportacion a PDF ...

--- src/main/java/mx/com/ferbo/controller/ReporteInventarioBean.java
+++ src/main/java/mx/com/ferbo/controller/ReporteInventarioBean.java
@@ -1,37 +1,67 @@
 package mx.com.ferbo.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.Serializable;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.application.FacesMessage.Severity;
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
+import javax.persistence.EntityManager;
+
+import org.apache.log4j.Logger;
+import org.primefaces.PrimeFaces;
 
 import mx.com.ferbo.dao.CamaraDAO;
 import mx.com.ferbo.dao.ClienteDAO;
 import mx.com.ferbo.dao.PlantaDAO;
 import mx.com.ferbo.model.Camara;
 import mx.com.ferbo.model.Cliente;
+import mx.com.ferbo.model.Factura;
+import mx.com.ferbo.model.Partida;
 import mx.com.ferbo.model.Planta;
+import mx.com.ferbo.model.ServicioFactura;
+import mx.com.ferbo.util.EntityManagerUtil;
+import mx.com.ferbo.util.InventarioException;
+import mx.com.ferbo.util.JasperReportUtil;
+import mx.com.ferbo.util.conexion;
+import net.sf.jasperreports.engine.JRException;
 
 @Named
 @ViewScoped
 public class ReporteInventarioBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	private static Logger log = Logger.getLogger(ReporteInventarioBean.class);
+
 	private Date fecha;
 	private Integer idCliente;
+	private String pdf;
+	private String excel;
+	private String tipoFile;
 	
 	private Planta plantaSelect;
 	private Camara camaraSelect;
-	
+	private Partida p;
 	private List<Cliente> listaClientes;
 	private List<Planta> listaPlanta;
 	private List<Camara> listaCamara;
+	private List<String> listaFile;
+	private List<Partida> listaPartida;
+
 	
 	private ClienteDAO clienteDAO;
 	private PlantaDAO plantaDAO;
@@ -46,6 +76,7 @@ public class ReporteInventarioBean implements Serializable {
 		listaClientes = new ArrayList<Cliente>();
 		listaPlanta = new ArrayList<Planta>();
 		listaCamara = new ArrayList<Camara>();
+		listaFile = new ArrayList<>();
 		
 
 	}
@@ -57,14 +88,67 @@ public class ReporteInventarioBean implements Serializable {
 		listaClientes = clienteDAO.buscarHabilitados(true);
 		listaPlanta = plantaDAO.buscarTodos();
 		listaCamara = camaraDAO.buscarTodos();
-		//listaCamara = camaraDAO.buscarPorPlanta(plantaSelect);
+		
 	}
 	
 	public void filtradoCamara() {
 		listaCamara = camaraDAO.buscarPorPlanta(plantaSelect);
 		plantaSelect.setCamaraList(listaCamara);
 	}
+	
+	public void exportarPdf() throws JRException, IOException, SQLException{
+		System.out.println("Exportando a pdf.....");
+			String jasperPath = "/jasper/InventarioAlmacen.jrxml";
+			String filename = "InventarioAlmacen " + fecha + ".pdf";
+			String images = "/images/logo.jpeg";
+			String message = null;
+			Severity severity = null;
+			Partida pr = null;
+			File reportFile = new File(jasperPath);
+			File imgfile = null;
+			JasperReportUtil jasperReportUtil = new JasperReportUtil();
+			Map<String, Object> parameters = new HashMap<String, Object>();
+			Connection connection = null;
+			parameters = new HashMap<String, Object>();
+			try {
+				if (this.listaPartida == null || this.listaPartida.size() == 0)
+					throw new InventarioException("Debe seleccionar almenos un servicio");
+				if (this.idCliente== null)
+					throw new InventarioException("Debe seleccionar el cliente");
 
+				URL resource = getClass().getResource(jasperPath);
+				URL resourceimg = getClass().getResource(images);
+				String file = resource.getFile();
+				String img = resourceimg.getFile();
+				reportFile = new File(file);
+				imgfile = new File(img);
+				log.info(reportFile.getPath());
+				pr = new Partida();
+				Integer num = pr.getPartidaCve();
+				connection = EntityManagerUtil.getConnection();
+				parameters.put("REPORT_CONNECTION", connection);
+				parameters.put("idFactura", num);
+				parameters.put("imagen", imgfile.getPath());
+				log.info("Parametros: " + parameters.toString());
+				jasperReportUtil.createPdf(filename, parameters, reportFile.getPath());
+			} catch (Exception ex) {
+				ex.fillInStackTrace();
+				log.error("Problema general...", ex);
+				message = String.format("No se pudo imprimir el folio %s", this.p);
+				severity = FacesMessage.SEVERITY_INFO;
+				FacesContext.getCurrentInstance().addMessage(null,
+						new FacesMessage(severity, "Error en impresion", message));
+				PrimeFaces.current().ajax().update("form:messages", "form:dt-facturacionServicios");
+			} finally {
+				conexion.close((Connection) connection);
+			}
+
+		}
+	
+public void exportarExcel() throws JRException, IOException, SQLException{
+		System.out.println("Exportando a excel.....");
+	}
+	
 
 	public Date getFecha() {
 		return fecha;
@@ -109,6 +193,32 @@ public class ReporteInventarioBean implements Serializable {
 	}
 	public void setListaCamara(List<Camara> listaCamara) {
 		this.listaCamara = listaCamara;
+	}
+
+	
+	public String getPdf() {
+		return pdf;
+	}
+	public void setPdf(String pdf) {
+		this.pdf = pdf;
+	}
+	public String getExcel() {
+		return excel;
+	}
+	public void setExcel(String excel) {
+		this.excel = excel;
+	}
+	public List<String> getListaFile() {
+		return listaFile;
+	}
+	public void setListaFile(List<String> listaFile) {
+		this.listaFile = listaFile;
+	}
+	public String getTipoFile() {
+		return tipoFile;
+	}
+	public void setTipoFile(String tipoFile) {
+		this.tipoFile = tipoFile;
 	}
 
 }
