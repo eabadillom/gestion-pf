93e76711376208b9a0d293ea20b477d13b184e0d
judit_gs <judit.saldivar@hotmail.com>
2023-06-12 12:04:08 -0500

    Se suben cambios de la BD...

--- src/main/java/mx/com/ferbo/controller/ReporteInventarioBean.java
+++ src/main/java/mx/com/ferbo/controller/ReporteInventarioBean.java
@@ -2,6 +2,7 @@ package mx.com.ferbo.controller;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.Serializable;
 import java.net.URL;
 import java.sql.Connection;
@@ -11,8 +12,6 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
@@ -20,7 +19,7 @@ import javax.faces.application.FacesMessage.Severity;
 import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
-import javax.persistence.EntityManager;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
 import org.primefaces.PrimeFaces;
@@ -30,15 +29,23 @@ import mx.com.ferbo.dao.ClienteDAO;
 import mx.com.ferbo.dao.PlantaDAO;
 import mx.com.ferbo.model.Camara;
 import mx.com.ferbo.model.Cliente;
-import mx.com.ferbo.model.Factura;
-import mx.com.ferbo.model.Partida;
 import mx.com.ferbo.model.Planta;
-import mx.com.ferbo.model.ServicioFactura;
 import mx.com.ferbo.util.EntityManagerUtil;
-import mx.com.ferbo.util.InventarioException;
 import mx.com.ferbo.util.JasperReportUtil;
 import mx.com.ferbo.util.conexion;
+import mx.com.ferbo.utils.IOUtil;
 import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.design.JasperDesign;
+import net.sf.jasperreports.engine.export.ooxml.JRXlsxExporter;
+import net.sf.jasperreports.engine.xml.JRXmlLoader;
+import net.sf.jasperreports.export.OutputStreamExporterOutput;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
+import net.sf.jasperreports.export.SimpleXlsxReportConfiguration;
 
 @Named
 @ViewScoped
@@ -48,20 +55,14 @@ public class ReporteInventarioBean implements Serializable {
 	private static Logger log = Logger.getLogger(ReporteInventarioBean.class);
 
 	private Date fecha;
-	private Integer idCliente;
-	private String pdf;
-	private String excel;
-	private String tipoFile;
 	
 	private Planta plantaSelect;
 	private Camara camaraSelect;
-	private Partida p;
+	private Cliente clienteSelect;
+	
 	private List<Cliente> listaClientes;
 	private List<Planta> listaPlanta;
 	private List<Camara> listaCamara;
-	private List<String> listaFile;
-	private List<Partida> listaPartida;
-
 	
 	private ClienteDAO clienteDAO;
 	private PlantaDAO plantaDAO;
@@ -76,7 +77,6 @@ public class ReporteInventarioBean implements Serializable {
 		listaClientes = new ArrayList<Cliente>();
 		listaPlanta = new ArrayList<Planta>();
 		listaCamara = new ArrayList<Camara>();
-		listaFile = new ArrayList<>();
 		
 
 	}
@@ -84,6 +84,7 @@ public class ReporteInventarioBean implements Serializable {
 	public void init() {
 		plantaSelect = new Planta();
 		camaraSelect = new Camara();
+		clienteSelect = new Cliente();
 		
 		listaClientes = clienteDAO.buscarHabilitados(true);
 		listaPlanta = plantaDAO.buscarTodos();
@@ -99,23 +100,19 @@ public class ReporteInventarioBean implements Serializable {
 	public void exportarPdf() throws JRException, IOException, SQLException{
 		System.out.println("Exportando a pdf.....");
 			String jasperPath = "/jasper/InventarioAlmacen.jrxml";
-			String filename = "InventarioAlmacen " + fecha + ".pdf";
+			String filename = "InventarioAlmacen" +fecha+".pdf";
 			String images = "/images/logo.jpeg";
 			String message = null;
 			Severity severity = null;
-			Partida pr = null;
 			File reportFile = new File(jasperPath);
 			File imgfile = null;
 			JasperReportUtil jasperReportUtil = new JasperReportUtil();
 			Map<String, Object> parameters = new HashMap<String, Object>();
 			Connection connection = null;
 			parameters = new HashMap<String, Object>();
+			
 			try {
-				if (this.listaPartida == null || this.listaPartida.size() == 0)
-					throw new InventarioException("Debe seleccionar almenos un servicio");
-				if (this.idCliente== null)
-					throw new InventarioException("Debe seleccionar el cliente");
-
+			
 				URL resource = getClass().getResource(jasperPath);
 				URL resourceimg = getClass().getResource(images);
 				String file = resource.getFile();
@@ -123,21 +120,73 @@ public class ReporteInventarioBean implements Serializable {
 				reportFile = new File(file);
 				imgfile = new File(img);
 				log.info(reportFile.getPath());
-				pr = new Partida();
-				Integer num = pr.getPartidaCve();
+			
+				Integer clienteCve = clienteSelect.getCteCve();
+				Integer camaraCve = camaraSelect.getCamaraCve();
+				Integer plantaCve = plantaSelect.getPlantaCve();
+			
 				connection = EntityManagerUtil.getConnection();
 				parameters.put("REPORT_CONNECTION", connection);
-				parameters.put("idFactura", num);
+				parameters.put("idCliente",clienteCve );
+				parameters.put("Camara", camaraCve);
+				parameters.put("Planta", plantaCve);
+				parameters.put("Fecha",fecha );
 				parameters.put("imagen", imgfile.getPath());
 				log.info("Parametros: " + parameters.toString());
 				jasperReportUtil.createPdf(filename, parameters, reportFile.getPath());
 			} catch (Exception ex) {
-				ex.fillInStackTrace();
 				log.error("Problema general...", ex);
-				message = String.format("No se pudo imprimir el folio %s", this.p);
+				message = String.format("No se pudo imprimir el reporte");
 				severity = FacesMessage.SEVERITY_INFO;
-				FacesContext.getCurrentInstance().addMessage(null,
-						new FacesMessage(severity, "Error en impresion", message));
+				FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(severity, "Error en impresion", message));
+				PrimeFaces.current().ajax().update("form:messages", "form:dt-facturacionServicios");
+			} finally {
+				conexion.close((Connection) connection);
+			}
+
+		}
+	public void exportarExcel() throws JRException, IOException, SQLException{
+		System.out.println("Exportando a excel.....");
+			String jasperPath = "/jasper/InventarioAlmacen.jrxml";
+			String filename = "InventarioAlmacen" +fecha+".xlsx";
+			String images = "/images/logo.jpeg";
+			String message = null;
+			Severity severity = null;
+			File reportFile = new File(jasperPath);
+			File imgfile = null;
+			JasperReportUtil jasperReportUtil = new JasperReportUtil();
+			Map<String, Object> parameters = new HashMap<String, Object>();
+			Connection connection = null;
+			parameters = new HashMap<String, Object>();
+			
+			try {
+			
+				URL resource = getClass().getResource(jasperPath);
+				URL resourceimg = getClass().getResource(images);
+				String file = resource.getFile();
+				String img = resourceimg.getFile();
+				reportFile = new File(file);
+				imgfile = new File(img);
+				log.info(reportFile.getPath());
+			
+				Integer clienteCve = clienteSelect.getCteCve();
+				Integer camaraCve = camaraSelect.getCamaraCve();
+				Integer plantaCve = plantaSelect.getPlantaCve();
+			
+				connection = EntityManagerUtil.getConnection();
+				parameters.put("REPORT_CONNECTION", connection);
+				parameters.put("idCliente",clienteCve );
+				parameters.put("Camara", camaraCve);
+				parameters.put("Planta", plantaCve);
+				parameters.put("Fecha",fecha );
+				parameters.put("imagen", imgfile.getPath());
+				log.info("Parametros: " + parameters.toString());
+				jasperReportUtil.createXlsx(filename, parameters, reportFile.getPath());
+			} catch (Exception ex) {
+				log.error("Problema general...", ex);
+				message = String.format("No se pudo imprimir el reporte");
+				severity = FacesMessage.SEVERITY_INFO;
+				FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(severity, "Error en impresion", message));
 				PrimeFaces.current().ajax().update("form:messages", "form:dt-facturacionServicios");
 			} finally {
 				conexion.close((Connection) connection);
@@ -145,24 +194,19 @@ public class ReporteInventarioBean implements Serializable {
 
 		}
 	
-public void exportarExcel() throws JRException, IOException, SQLException{
-		System.out.println("Exportando a excel.....");
-	}
+		
 	
-
 	public Date getFecha() {
 		return fecha;
 	}
-
-
 	public void setFecha(Date fecha) {
 		this.fecha = fecha;
 	}
-	public Integer getIdCliente() {
-		return idCliente;
+	public Cliente getClienteSelect() {
+		return clienteSelect;
 	}
-	public void setIdCliente(Integer idCliente) {
-		this.idCliente = idCliente;
+	public void setClienteSelect(Cliente clienteSelect) {
+		this.clienteSelect = clienteSelect;
 	}
 	public List<Cliente> getListaClientes() {
 		return listaClientes;
@@ -195,30 +239,6 @@ public void exportarExcel() throws JRException, IOException, SQLException{
 		this.listaCamara = listaCamara;
 	}
 
-	
-	public String getPdf() {
-		return pdf;
-	}
-	public void setPdf(String pdf) {
-		this.pdf = pdf;
-	}
-	public String getExcel() {
-		return excel;
-	}
-	public void setExcel(String excel) {
-		this.excel = excel;
-	}
-	public List<String> getListaFile() {
-		return listaFile;
-	}
-	public void setListaFile(List<String> listaFile) {
-		this.listaFile = listaFile;
-	}
-	public String getTipoFile() {
-		return tipoFile;
-	}
-	public void setTipoFile(String tipoFile) {
-		this.tipoFile = tipoFile;
-	}
+
 
 }
--- src/main/java/mx/com/ferbo/util/JasperReportUtil.java
+++ src/main/java/mx/com/ferbo/util/JasperReportUtil.java
@@ -17,6 +17,7 @@ import java.io.OutputStream;
 import java.nio.Buffer;
 
 import javassist.bytecode.ByteArray;
+import mx.com.ferbo.utils.IOUtil;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperExportManager;
@@ -26,7 +27,11 @@ import net.sf.jasperreports.engine.JasperReport;
 import net.sf.jasperreports.engine.design.JRDesignStyle;
 import net.sf.jasperreports.engine.design.JasperDesign;
 import net.sf.jasperreports.engine.export.JRPdfExporter;
+import net.sf.jasperreports.engine.export.ooxml.JRXlsxExporter;
 import net.sf.jasperreports.engine.xml.JRXmlLoader;
+import net.sf.jasperreports.export.OutputStreamExporterOutput;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
+import net.sf.jasperreports.export.SimpleXlsxReportConfiguration;
 
 public class JasperReportUtil {
 	public void createPdf(String fileName, Map<String, Object> parameters, String path) throws IOException {
@@ -49,12 +54,14 @@ public class JasperReportUtil {
 			report = JasperCompileManager.compileReport(design);
 			jasperPrint = JasperFillManager.fillReport(report, parameters);
 			JasperExportManager.exportReportToPdfStream(jasperPrint, output);
-			output.flush();
-			output.close();
+			
 			context.responseComplete();
 			FacesContext.getCurrentInstance().responseComplete();
 		} catch (JRException ex) {
 			ex.printStackTrace();
+		}finally {
+			output.flush();
+			output.close();
 		}
 	}
 	public StreamedContent getPdf(String fileName, Map<String, Object> parameters, String path) throws IOException {
@@ -76,6 +83,42 @@ public class JasperReportUtil {
 		}
 		
 		return respuesta;
+	}
+	
+	public void createXlsx(String fileName, Map<String, Object> parameters, String path) throws IOException {
+		
+		FacesContext context = null;
+		HttpServletResponse response = null;
+		OutputStream output = null;
+		JasperDesign design = null;
+		JasperReport report = null;
+		JasperPrint jasperPrint = null;
+
+		try {
+			context = FacesContext.getCurrentInstance();
+			response = (HttpServletResponse) context.getExternalContext().getResponse();
+			output = response.getOutputStream();
+			String disposition = String.format("attachment; filename=\"%s\"", fileName);
+			response.setHeader("Content-Disposition", disposition);
+			response.addHeader("Content-Disposition", disposition);
+			response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+			design = JRXmlLoader.load(path);
+			report = JasperCompileManager.compileReport(design);
+			jasperPrint = JasperFillManager.fillReport(report, parameters);
+			parameters.put("IS_IGNORE_PAGINATION", true);
+			JRXlsxExporter exporter = new JRXlsxExporter();
+			SimpleXlsxReportConfiguration reportConfig = new SimpleXlsxReportConfiguration();
+			OutputStreamExporterOutput outputExporter = new SimpleOutputStreamExporterOutput(output);
+			exporter.setExporterOutput(outputExporter);
+			exporter.setConfiguration(reportConfig);
+			JasperExportManager.exportReportToPdfStream(jasperPrint, output);
+			context.responseComplete();
+			FacesContext.getCurrentInstance().responseComplete();
+		} catch (JRException ex) {
+			ex.printStackTrace();
+		} finally {
+			IOUtil.close(output);
+		}
 	}
 
 }
