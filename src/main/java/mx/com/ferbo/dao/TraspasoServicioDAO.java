a526219e7c36bbade8ae3beb1b4c4ba3703dc982
Giovanny2999 <monti290399@hotmail.com>
2023-07-24 11:04:24 -0600

    Correcion de libreria logger

--- src/main/java/mx/com/ferbo/dao/TraspasoServicioDAO.java
+++ src/main/java/mx/com/ferbo/dao/TraspasoServicioDAO.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 
-import org.jfree.util.Log;
+import org.apache.log4j.Logger;
 
 import mx.com.ferbo.commons.dao.IBaseDAO;
 import mx.com.ferbo.model.ConstanciaTraspaso;
@@ -13,6 +13,8 @@ import mx.com.ferbo.model.TraspasoServicio;
 import mx.com.ferbo.util.EntityManagerUtil;
 
 public class TraspasoServicioDAO extends IBaseDAO<TraspasoServicioDAO, Integer>{
+	
+	private static Logger log = Logger.getLogger(TraspasoServicioDAO.class);
 
 	@Override
 	public TraspasoServicioDAO buscarPorId(Integer id) {
@@ -30,7 +32,7 @@ public class TraspasoServicioDAO extends IBaseDAO<TraspasoServicioDAO, Integer>{
 			setParameter("traspaso", ct.getId()).
 			getResultList();
 		} catch (Exception e) {
-			Log.error("Problema al buscar por contsancia traspaso servicio", e);
+			log.error("Problema al buscar por contsancia traspaso servicio", e);
 		}finally {
 			EntityManagerUtil.close(em);
 		}
