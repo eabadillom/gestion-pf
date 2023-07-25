6c976b0e17ef3aa91bee5b4fed2b6671cdaec7b1
judit_gs <judit.saldivar@hotmail.com>
2023-07-25 09:47:52 -0500

    Se actualizan DAO'S y pantalla

--- src/main/java/mx/com/ferbo/dao/PosicionCamaraDAO.java
+++ src/main/java/mx/com/ferbo/dao/PosicionCamaraDAO.java
@@ -8,6 +8,7 @@ import javax.persistence.Query;
 
 import org.apache.log4j.Logger;
 
+import groovy.xml.Entity;
 import mx.com.ferbo.commons.dao.IBaseDAO;
 import mx.com.ferbo.model.Camara;
 import mx.com.ferbo.model.Posicion;
@@ -15,29 +16,37 @@ import mx.com.ferbo.util.EntityManagerUtil;
 import mx.com.ferbo.util.JPAEntity;
 
 public class PosicionCamaraDAO extends IBaseDAO<Posicion, Integer>{
-	
 	EntityManager entity = JPAEntity.getEntity().createEntityManager();
-	
 	private static Logger log = Logger.getLogger(PosicionCamaraDAO.class);
 	
 	@SuppressWarnings("unchecked")
 	public List<Posicion> findAll(){
-		List<Posicion> posiciones;
-		Query sql = entity.createNamedQuery("Posicion.findAll", Posicion.class);
-		posiciones = sql.getResultList();
-		System.out.println(posiciones + "*****************************************************");
+		List<Posicion> posiciones = null;
+		EntityManager em = null;
+		try {
+			em = EntityManagerUtil.getEntityManager();
+			Query sql = entity.createNamedQuery("Posicion.findAll", Posicion.class);
+			posiciones = sql.getResultList();
+			System.out.println(posiciones + "*****************************************************");
+		}catch(Exception e) {
+			log.error("Error al obtener informacion",e);
+		}finally {
+			EntityManagerUtil.close(em);
+		}
 		return posiciones;
 	}
 	
 	public String save(Posicion sP) {
+		EntityManager entity = null;
 		try {
-			EntityManager entity = EntityManagerUtil.getEntityManager();
+			entity = EntityManagerUtil.getEntityManager();
 			entity.getTransaction().begin();
 			entity.persist(sP);
-			entity.getTransaction().commit();
-			entity.close();
-		} catch (Exception e) {
-			return e.getMessage();
+			entity.getTransaction().commit();			
+		}catch(Exception e) {
+			log.error("Error al obtener informacion", e);
+		}finally {
+			EntityManagerUtil.close(entity);
 		}
 		return null;
 	}
@@ -49,17 +58,33 @@ public class PosicionCamaraDAO extends IBaseDAO<Posicion, Integer>{
 	}
 
 	public List<Posicion> buscarPorCamara(Camara c) {
-	List<Posicion> listaP = null;
-	EntityManager em = EntityManagerUtil.getEntityManager();
-	listaP = em.createNamedQuery("Posicion.findByCamara", Posicion.class)
-			.setParameter("camaraCve", c.getCamaraCve()).getResultList();
+		List<Posicion> listaP = null;
+		EntityManager em = null;
+			try {
+				em = EntityManagerUtil.getEntityManager();
+				listaP = em.createNamedQuery("Posicion.findByCamara", Posicion.class)
+						.setParameter("camaraCve", c.getCamaraCve()).getResultList();
+		}catch(Exception e) {
+			log.error("Error al obtener informacion",e);
+		}finally {
+			EntityManagerUtil.close(em);
+		}
 	return listaP;
 	
 	}
 	@Override
 	public List<Posicion> buscarTodos() {
-		List<Posicion> posiciones = new ArrayList<Posicion>();
-		System.out.println(posiciones + "*****************************************************");
+		List<Posicion> posiciones = null;
+		EntityManager em = null;
+		try {
+			em = EntityManagerUtil.getEntityManager();
+			posiciones = new ArrayList<Posicion>();
+			System.out.println(posiciones + "*****************************************************");
+		}catch(Exception e) {
+			log.error("Error al obtener informacion",e);
+		}finally {
+			EntityManagerUtil.close(em);
+		}
 		return posiciones;
 	}
 
@@ -75,9 +100,7 @@ public class PosicionCamaraDAO extends IBaseDAO<Posicion, Integer>{
 					.createNamedQuery("Posicion.findByPlantaCamara", Posicion.class)
 					.setParameter("plantaCve", e.getPlanta().getPlantaCve())
 					.setParameter("camaraCve", e.getCamara().getCamaraCve());
-			
 			posiciones = query.getResultList();
-			
 		} catch (Exception ex) {
 			log.error("Problema para obtener el listado de posiciones por cámara...", ex);
 		} finally {
@@ -88,44 +111,47 @@ public class PosicionCamaraDAO extends IBaseDAO<Posicion, Integer>{
 	}
 
 	@Override
-	public String actualizar(Posicion e) {
-		
-		try {
-			EntityManager entity = EntityManagerUtil.getEntityManager();
-			entity.getTransaction().begin();
-			Query actualizar = entity.createNativeQuery(" UPDATE posicion SET habilitada  = :habil WHERE id_posicion = :pos ") ;
-			actualizar.setParameter("habil", (e.getHabilitada() == true) ? 1 : 0);
-			actualizar.setParameter("pos", e.getIdPosicion());
-			actualizar.executeUpdate();
-			entity.getTransaction().commit();
-			entity.close();
-		} catch (Exception e2) {
-			return e2.getMessage();
+	public String actualizar(Posicion p) {
+		EntityManager entity = null;
+	try {
+		entity = EntityManagerUtil.getEntityManager();
+		entity.getTransaction().begin();
+		Query actualizar = entity.createNativeQuery(" UPDATE posicion SET habilitada  = :habil WHERE id_posicion = :pos ") ;
+		actualizar.setParameter("habil", (p.getHabilitada() == true) ? 1 : 0);
+		actualizar.setParameter("pos", p.getIdPosicion());
+		actualizar.executeUpdate();
+		entity.getTransaction().commit();
+		}catch(Exception e) {
+			log.error("Error al obtener informacion",e);
+		}finally {
+			EntityManagerUtil.close(entity);
 		}
 		return null;
 	}
 
 	@Override
-	public String guardar(Posicion sP) {
-		System.out.println(sP + "GURADAR---------------------------------");
-		try {
-			EntityManager entity = EntityManagerUtil.getEntityManager();
-			entity.getTransaction().begin();
-			//entity.persist(sP);
-			Query otra = entity.createNativeQuery(" insert into posicion(id_planta, id_camara, cod_posicion, desc_posicion, temp_ini, temp_fin, habilitada) "
-					+ "values(?,?,?,?,?,?,?); "); 
-			otra.setParameter(1, sP.getPlanta().getPlantaCve());
-			otra.setParameter(2, sP.getCamara().getCamaraCve());
-			otra.setParameter(3, sP.getCodPosicion());
-			otra.setParameter(4, sP.getDescPosicion());
-			otra.setParameter(5, sP.getTempIni());
-			otra.setParameter(6, sP.getTempFin());
-			otra.setParameter(7, sP.getHabilitada());
-			otra.executeUpdate();
-			entity.getTransaction().commit();
-			entity.close();
-		} catch (Exception e) {
-			return e.getMessage();
+	public String guardar(Posicion p) {
+		EntityManager entity = null;
+	try {
+		entity = EntityManagerUtil.getEntityManager();
+		entity.getTransaction().begin();
+		//entity.persist(sP);
+		Query otra = entity.createNativeQuery(" insert into posicion(id_planta, id_camara, cod_posicion, desc_posicion, temp_ini, temp_fin, habilitada) "
+				+ "values(?,?,?,?,?,?,?); "); 
+		otra.setParameter(1, p.getPlanta().getPlantaCve());
+		otra.setParameter(2, p.getCamara().getCamaraCve());
+		otra.setParameter(3, p.getCodPosicion());
+		otra.setParameter(4, p.getDescPosicion());
+		otra.setParameter(5, p.getTempIni());
+		otra.setParameter(6, p.getTempFin());
+		otra.setParameter(7, p.getHabilitada());
+		otra.executeUpdate();
+		entity.getTransaction().commit();
+		System.out.println(p + "GURADAR---------------------------------");
+		}catch(Exception e) {
+			log.error("Error al obtener informacion", e);
+		}finally {
+			EntityManagerUtil.close(entity);
 		}
 		return null;
 	}
