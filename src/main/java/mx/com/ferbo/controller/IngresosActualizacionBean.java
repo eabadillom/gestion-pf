f990acde64daf42bae60c0f27111a3f23579d6ee
Giovanny2999 <monti290399@hotmail.com>
2023-07-26 12:53:46 -0600

    Correccion ingresos alta

--- src/main/java/mx/com/ferbo/controller/IngresosActualizacionBean.java
+++ src/main/java/mx/com/ferbo/controller/IngresosActualizacionBean.java
@@ -1,5 +1,10 @@
 package mx.com.ferbo.controller;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.application.FacesMessage.Severity;
@@ -10,21 +15,61 @@ import javax.inject.Named;
 import org.apache.log4j.Logger;
 import org.primefaces.PrimeFaces;
 
+import mx.com.ferbo.dao.BancoDAO;
+import mx.com.ferbo.dao.ClienteDAO;
+import mx.com.ferbo.dao.PagoDAO;
+import mx.com.ferbo.dao.TipoPagoDAO;
+import mx.com.ferbo.model.Bancos;
+import mx.com.ferbo.model.Cliente;
+import mx.com.ferbo.model.Pago;
+import mx.com.ferbo.model.TipoPago;
+
 @Named
 @ViewScoped
 
-public class IngresosActualizacionBean {
+public class IngresosActualizacionBean implements Serializable{
 
 	private static final long serialVersionUID = -626048119540963939L;
 	private static Logger log = Logger.getLogger(IngresosActualizacionBean.class);
 	
+	private Date startDate;
+	private Date endDate;	
+	
+	private PagoDAO pagofactDAO;
+	private ClienteDAO clienteDAO;
+	private TipoPagoDAO tipoPagoDAO;
+	private BancoDAO bancoDAO;
+	
+	
+	private List<Pago> listaPago;
+	private List<Cliente> listaCtes;
+	private List<TipoPago> listatipoPago;
+	private List<Bancos> listaBancos;
+	
+	private Pago pagoSelected;
+	private Cliente cteSelect;
 	
 	public IngresosActualizacionBean() {
+		clienteDAO = new ClienteDAO();
+		pagofactDAO = new PagoDAO();
+		tipoPagoDAO = new TipoPagoDAO();
+		bancoDAO = new BancoDAO();
+		
+		listaPago = new ArrayList<Pago>();
+		listaCtes = new ArrayList<Cliente>();
+		
+		pagoSelected = new Pago();
+		cteSelect = new Cliente();
+		
 		
 	}
 	
 	@PostConstruct
 	public void init(){
+		
+		listaCtes = clienteDAO.findall();
+		listaBancos = bancoDAO.buscarTodos();
+		listatipoPago = tipoPagoDAO.buscarTodos();
 		
 	}
 	
@@ -81,6 +126,72 @@ public class IngresosActualizacionBean {
 		listaPago = pagofactDAO.buscaPorClienteFechas(cteSelect, startDate, endDate);
 		
 	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+	public List<Pago> getListaPago() {
+		return listaPago;
+	}
+
+	public void setListaPago(List<Pago> listaPago) {
+		this.listaPago = listaPago;
+	}
+
+	public Pago getPagoSelected() {
+		return pagoSelected;
+	}
+
+	public void setPagoSelected(Pago pagoSelected) {
+		this.pagoSelected = pagoSelected;
+	}
+
+	public Cliente getCteSelect() {
+		return cteSelect;
+	}
+
+	public void setCteSelect(Cliente cteSelect) {
+		this.cteSelect = cteSelect;
+	}
+
+	public List<Cliente> getListaCtes() {
+		return listaCtes;
+	}
+
+	public void setListaCtes(List<Cliente> listaCtes) {
+		this.listaCtes = listaCtes;
+	}
+
+	public List<TipoPago> getListatipoPago() {
+		return listatipoPago;
+	}
+
+	public void setListatipoPago(List<TipoPago> listatipoPago) {
+		this.listatipoPago = listatipoPago;
+	}
+
+	public List<Bancos> getListaBancos() {
+		return listaBancos;
+	}
+
+	public void setListaBancos(List<Bancos> listaBancos) {
+		this.listaBancos = listaBancos;
+	}
+	
+	
 	
 	
 }
--- src/main/java/mx/com/ferbo/controller/IngresosCrudBean.java
+++ src/main/java/mx/com/ferbo/controller/IngresosCrudBean.java
@@ -60,8 +60,7 @@ public class IngresosCrudBean implements Serializable {
 		private Date fecha;
 		private Integer bancoCve;
 		private Integer tipoP;
-		private Date startDate;
-		private Date endDate;
+		
 		
 		private Cliente cteSelect;
 		private Factura facturaSelect;
@@ -210,7 +209,13 @@ public class IngresosCrudBean implements Serializable {
 			String message = null;
 			Severity severity = null;
 			try {
-				facturaDAO.actualizar(facturaSelect);
+				
+				if(!listaPago.isEmpty()) {//DUDA SOLO SE GUARDAN DATOS EN PAGO????
+					for(Pago p: listaPago) {					
+						pagofactDAO.guardar(p);					
+					}
+				}
+				//facturaDAO.actualizar(facturaSelect);//ERROR GUARDA LA ULTIMA FACTURA SELECCIONADA Y NO TODOS LOS REGISTROS
 				severity = FacesMessage.SEVERITY_INFO;
 				message = "El pago se genero correctamente";
 			} catch (Exception e) {
@@ -224,60 +229,7 @@ public class IngresosCrudBean implements Serializable {
 				PrimeFaces.current().ajax().update("form:messages", "form:dt-ingAlta", "form:dt-Factura");
 				
 			}
-	}
-		
-		public void updatePago() {
-			
-			System.out.println(pagoSelected);
-			
-			String messages = null;
-			Severity severity = null;		
-			
-			try {
-				
-				if(pagofactDAO.actualizar(pagoSelected)==null) {
-					severity = FacesMessage.SEVERITY_INFO;
-					messages = "El pago fue actualizado";
-				}
-				
-			} catch (Exception e) {
-				severity = FacesMessage.SEVERITY_ERROR;
-				messages = "Error al actualizar pago";
-				log.error(e.getMessage());
-			}
-			
-			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(severity, "Actualización", messages));
-			PrimeFaces.current().ajax().update("form:messages");
-		}
-		
-		public void deletePago() {
-			
-			String mensaje = null;
-			Severity severity = null;
-			
-			try {
-				
-				if(pagofactDAO.eliminar(pagoSelected)==null) {
-					mensaje = "El pago fue eliminado correctamente";
-					severity = FacesMessage.SEVERITY_INFO;
-					filtraPagos();
-				}
-				
-			} catch (Exception e) {
-				mensaje = "Ocurrio un error al querer eliminar el pago";
-				severity = FacesMessage.SEVERITY_ERROR;
-				log.error(e.getMessage());
-			}
-			
-			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(severity, "Eliminar", mensaje));
-			PrimeFaces.current().ajax().update("form:messages");
-		}
-		
-		public void filtraPagos() {
-			
-			listaPago = pagofactDAO.buscaPorClienteFechas(cteSelect, startDate, endDate);
-			
-		}
+	}		
 
 		public Integer getIdCte() {
 			return idCte;
@@ -471,23 +423,7 @@ public class IngresosCrudBean implements Serializable {
 
 		public void setTipoP(Integer tipoP) {
 			this.tipoP = tipoP;
-		}
-
-		public Date getStartDate() {
-			return startDate;
-		}
-
-		public void setStartDate(Date startDate) {
-			this.startDate = startDate;
-		}
-
-		public Date getEndDate() {
-			return endDate;
-		}
-
-		public void setEndDate(Date endDate) {
-			this.endDate = endDate;
-		}
+		}		
 
 		public Pago getPagoSelected() {
 			return pagoSelected;
