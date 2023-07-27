e188a17939b490cd6218c8a301a283a44d147dc5
judit_gs <judit.saldivar@hotmail.com>
2023-07-27 11:41:04 -0500

    Actualizacion DAO'S

--- src/main/java/mx/com/ferbo/dao/SerieFacturaDAO.java
+++ src/main/java/mx/com/ferbo/dao/SerieFacturaDAO.java
@@ -5,72 +5,106 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 
+import org.apache.log4j.Logger;
+import org.jfree.util.Log;
+
+import groovy.xml.Entity;
 import mx.com.ferbo.model.SerieFactura;
 import mx.com.ferbo.model.StatusSerie;
 import mx.com.ferbo.util.EntityManagerUtil;
 
 public class SerieFacturaDAO {
+	private static Logger log = Logger.getLogger(SerieFacturaDAO.class);
 
 	public List<SerieFactura> findAll() {
-		EntityManager entity = EntityManagerUtil.getEntityManager();
+		EntityManager entity = null;
 		List<SerieFactura> list = null;
-		list = entity.createNamedQuery("SerieFactura.findAll", SerieFactura.class).getResultList();
+		
+		try {
+			entity = EntityManagerUtil.getEntityManager();
+			list = entity.createNamedQuery("SerieFactura.findAll", SerieFactura.class).getResultList();
+		}catch(Exception e) {
+			log.error("Error al obtener informacion",e);
+		}finally {
+			EntityManagerUtil.close(entity);
+		}
 		return list;
 	};
 
 	public List<StatusSerie> findStatus() {
 		EntityManager entity = EntityManagerUtil.getEntityManager();
 		List<StatusSerie> list = null;
+	try {
+		entity = EntityManagerUtil.getEntityManager();
 		list = entity.createNamedQuery("StatusSerie.findAll", StatusSerie.class).getResultList();
+		}catch(Exception e) {
+			log.error("Error al obtener informacion",e);
+		}finally {
+			EntityManagerUtil.close(entity);
+		}
+	
 		return list;
 	};
 
 	public String save(SerieFactura sF) {
+		EntityManager entity = null;
 		try {
-			EntityManager entity = EntityManagerUtil.getEntityManager();
+			 entity = EntityManagerUtil.getEntityManager();
 			entity.getTransaction().begin();
 			entity.persist(sF);
 			entity.getTransaction().commit();
 			entity.close();
 		} catch (Exception e) {
 			return e.getMessage();
+		}finally {
+			EntityManagerUtil.close(entity);
 		}
 		return null;
 	};
 
 	public String update(SerieFactura sF) {
+		EntityManager entity = null;
 		try {
-			EntityManager entity = EntityManagerUtil.getEntityManager();
+			 entity = EntityManagerUtil.getEntityManager();
 			entity.getTransaction().begin();
 			entity.merge(sF);
 			entity.getTransaction().commit();
-			entity.close();
 		} catch (Exception e) {
 			return e.getMessage();
+		}finally {
+			EntityManagerUtil.close(entity);
 		}
 		return null;
 	};
 
 	public String cancelar(int id) {
-		EntityManager entity = EntityManagerUtil.getEntityManager();
+		EntityManager entity = null;
+		try {
+		 entity = EntityManagerUtil.getEntityManager();
 		entity.getTransaction().begin();
 		Query sql = entity.createNativeQuery("update serie_factura set status_serie = 3 where id = ?;");
 		sql.setParameter(1, id);
 		sql.executeUpdate();
 		entity.getTransaction().commit();
-		entity.close();
+		}catch(Exception e) {
+			log.error("Error al obtener informacion",e);
+		}finally {
+			EntityManagerUtil.close(entity);
+		}
 		return null;
 	};
 
 	public String delete(SerieFactura sF) {
+		EntityManager entity = null;
 		try {
-			EntityManager entity = EntityManagerUtil.getEntityManager();
+			 entity = EntityManagerUtil.getEntityManager();
 			entity.getTransaction().begin();
 			entity.remove(entity.merge(sF));
 			entity.getTransaction().commit();
-			entity.close();
 		} catch (Exception e) {
 			return e.getMessage();
+		}finally {
+			EntityManagerUtil.close(entity);
 		}
 		return null;
 	};
--- src/main/java/mx/com/ferbo/dao/SerieNotaDAO.java
+++ src/main/java/mx/com/ferbo/dao/SerieNotaDAO.java
@@ -5,72 +5,104 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 
+import org.apache.log4j.Logger;
+import org.jfree.util.Log;
+
 import mx.com.ferbo.model.SerieNota;
 import mx.com.ferbo.model.StatusSerie;
 import mx.com.ferbo.util.EntityManagerUtil;
 
 public class SerieNotaDAO {
+	private static Logger log = Logger.getLogger(PosicionCamaraDAO.class);
 
-	public List<SerieNota> findAll() {
-		EntityManager entity = EntityManagerUtil.getEntityManager();
+	public List<SerieNota> findAll() {		
+		EntityManager entity = null;
 		List<SerieNota> list = null;
-		list = entity.createNamedQuery("SerieNota.findAll", SerieNota.class).getResultList();
+		try {
+			entity = EntityManagerUtil.getEntityManager();
+			list = entity.createNamedQuery("SerieNota.findAll", SerieNota.class).getResultList();
+		}catch(Exception e) {
+			log.error("Error al intentar obtener informacion",e);
+		}finally {
+			EntityManagerUtil.close(entity);
+		}
 		return list;
 	};
 
 	public List<StatusSerie> findStatus() {
-		EntityManager entity = EntityManagerUtil.getEntityManager();
+		EntityManager entity = null;
 		List<StatusSerie> list = null;
-		list = entity.createNamedQuery("StatusSerie.findAll", StatusSerie.class).getResultList();
+		try {
+			entity = EntityManagerUtil.getEntityManager();
+			list = entity.createNamedQuery("StatusSerie.findAll", StatusSerie.class).getResultList();			
+		}catch(Exception e) {
+			log.error("Error al intentar obtener informacion",e);
+		}finally {
+			EntityManagerUtil.close(entity);
+		}
 		return list;
 	};
 
 	public String save(SerieNota sN) {
+		EntityManager entity= null;
 		try {
-			EntityManager entity = EntityManagerUtil.getEntityManager();
+			 entity = EntityManagerUtil.getEntityManager();
 			entity.getTransaction().begin();
 			entity.persist(sN);
 			entity.getTransaction().commit();
 			entity.close();
 		} catch (Exception e) {
 			return e.getMessage();
+		}finally {
+			EntityManagerUtil.close(entity);
 		}
 		return null;
 	};
 
 	public String update(SerieNota sN) {
+		EntityManager entity = null;
 		try {
-			EntityManager entity = EntityManagerUtil.getEntityManager();
+			 entity = EntityManagerUtil.getEntityManager();
 			entity.getTransaction().begin();
 			entity.merge(sN);
 			entity.getTransaction().commit();
 			entity.close();
 		} catch (Exception e) {
 			return e.getMessage();
+		}finally {
+			EntityManagerUtil.close(entity);
 		}
 		return null;
 	};
 
 	public String cancelar(int id) {
-		EntityManager entity = EntityManagerUtil.getEntityManager();
+		EntityManager entity = null;
+		try {
+		 entity = EntityManagerUtil.getEntityManager();
 		entity.getTransaction().begin();
 		Query sql = entity.createNativeQuery("update SERIE_NOTA set STATUS_SERIE = 3 where ID = ?;");
 		sql.setParameter(1, id);
 		sql.executeUpdate();
 		entity.getTransaction().commit();
-		entity.close();
+		}catch(Exception e) {
+			log.error("Error al obtener informacion",e);
+		}finally {
+			EntityManagerUtil.close(entity);
+		}
 		return null;
 	};
 
 	public String delete(SerieNota sN) {
+		EntityManager entity = null;
 		try {
-			EntityManager entity = EntityManagerUtil.getEntityManager();
+			 entity = EntityManagerUtil.getEntityManager();
 			entity.getTransaction().begin();
 			entity.remove(entity.merge(sN));
 			entity.getTransaction().commit();
-			entity.close();
 		} catch (Exception e) {
 			return e.getMessage();
+		}finally {
+			EntityManagerUtil.close(entity);
 		}
 		return null;
 	};
