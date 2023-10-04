d94d44c8ce9f304aacbee0e8f2bb2b6193835b9a
Esteban Badillo <ebadillom@gmail.com>
2023-10-04 16:52:47 -0600

    Se agrega consulta de reporte de entradas por DataTable en la interfaz.

---
+++ src/main/java/mx/com/ferbo/dao/RepEntradasDAO.java
@@ -0,0 +1,155 @@
+package mx.com.ferbo.dao;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import mx.com.ferbo.commons.dao.IBaseDAO;
+import mx.com.ferbo.ui.RepEntradas;
+import mx.com.ferbo.util.EntityManagerUtil;
+
+public class RepEntradasDAO extends IBaseDAO<RepEntradas, Integer>{
+	private static Logger log = LogManager.getLogger(RepEntradasDAO.class);
+
+	@Override
+	public RepEntradas buscarPorId(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<RepEntradas> buscarTodos() {
+		
+		return null;
+	}
+
+	@Override
+	public List<RepEntradas> buscarPorCriterios(RepEntradas e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<RepEntradas> buscar(Date fechaIni, Date fechaFin, Integer idCliente, Integer idPlanta, Integer idCamara) {
+		List<RepEntradas> resultList = null;
+		EntityManager entity = null;
+		String sql = null;
+		
+		try {
+			sql = "SELECT "
+					+ "	cdd.folio_cliente, "
+					+ "	cdd.fecha_ingreso, "
+					+ "	c.numero_cte, "
+					+ "	c.cte_nombre, "
+					+ "	p.PESO_TOTAL, "
+					+ "	p.CANTIDAD_TOTAL, "
+					+ "	p.valorMercancia, "
+					+ "	dp.dtp_codigo, "
+					+ "	dp.dtp_SAP, "
+					+ "	prd.producto_ds, "
+					+ "	dp.dtp_lote, "
+					+ "	dp.dtp_po, "
+					+ "	dp.dtp_caducidad, "
+					+ "	dp.dtp_tarimas, "
+					+ "	dp.cantidad_u_manejo, "
+					+ "	udm.unidad_de_manejo_ds, "
+					+ "	plt.planta_cve, "
+					+ "	plt.planta_abrev AS planta, "
+					+ "	cam.camara_cve, "
+					+ "	cam.camara_abrev AS camara "
+					+ "FROM PARTIDA p INNER JOIN CONSTANCIA_DE_DEPOSITO cdd ON cdd.folio = p.folio "
+					+ "INNER JOIN CLIENTE c ON c.cte_cve = cdd.cte_cve "
+					+ "INNER JOIN DETALLE_PARTIDA dp ON dp.partida_cve = p.partida_cve AND dp.DET_PART_CVE = 1 "
+					+ "INNER JOIN UNIDAD_DE_PRODUCTO udp ON udp.unidad_de_producto_cve = p.unidad_de_producto_cve "
+					+ "INNER JOIN UNIDAD_DE_MANEJO udm ON udm.unidad_de_manejo_cve = udp.UNIDAD_DE_MANEJO_CVE "
+					+ "INNER JOIN PRODUCTO prd ON prd.producto_cve = udp.producto_cve "
+					+ "INNER JOIN CAMARA cam ON cam.camara_cve = p.camara_cve "
+					+ "INNER JOIN PLANTA plt ON cam.planta_cve = plt.planta_cve "
+					+ "WHERE cdd.FECHA_INGRESO BETWEEN :fechaIni AND :fechaFin "
+					+ "	AND (cdd.cte_cve = :idCliente OR :idCliente IS NULL) "
+					+ "	AND (cam.camara_cve = :idCamara OR :idCamara IS NULL) "
+					+ "	AND (plt.planta_cve = :idPlanta OR :idPlanta IS NULL) "
+					+ "ORDER BY "
+					+ "	c.cte_nombre DESC, "
+					+ "	c.numero_cte DESC, "
+					+ "	prd.producto_ds DESC, "
+					+ "	cdd.fecha_ingreso DESC ";
+			entity = EntityManagerUtil.getEntityManager();
+			
+			List<Object[]> results = entity.createNativeQuery(sql)
+					.setParameter("fechaIni", fechaIni)
+					.setParameter("fechaFin", fechaFin)
+					.setParameter("idCliente", idCliente)
+					.setParameter("idPlanta", idPlanta)
+					.setParameter("idCamara", idCamara)
+					.getResultList()
+					;
+			
+			resultList = new ArrayList<RepEntradas>();
+			for(Object[] o : results) {
+				RepEntradas r = new RepEntradas();
+				int idx = 0;
+				r.setFolioCliente((String) o[idx++]);
+				r.setFechaIngreso((Date) o[idx++]);
+				r.setNumeroCliente((String) o[idx++]);
+				r.setNombreCliente((String) o[idx++]);
+				r.setPesoTotal((BigDecimal) o[idx++]);
+				r.setCantidadTotal((Integer) o[idx++]);
+				r.setValorMercancia((BigDecimal) o[idx++]);
+				r.setDtpCodigo((String) o[idx++]);
+				r.setDtpSap((String) o[idx++]);
+				r.setProductoDescripcion((String) o[idx++]);
+				r.setDtpLote((String) o[idx++]);
+				r.setDtpPo((String) o[idx++]);
+				r.setDtpCaducidad((Date) o[idx++]);
+				r.setDtpTarimas((String) o[idx++]);
+				r.setCantidadUnidadManejo((Integer) o[idx++]);
+				r.setUnidadManejoDescripcion((String) o[idx++]);
+				r.setIdPlanta((Integer) o[idx++]);
+				r.setNombrePlanta((String) o[idx++]);
+				r.setIdCamara((Integer) o[idx++]);
+				r.setNombreCamara((String) o[idx++]);
+				
+				resultList.add(r);
+			}
+			
+		} catch(Exception ex) {
+			log.error("Problema para obtener el reporte de Entradas...", ex);
+		} finally {
+			EntityManagerUtil.close(entity);
+		}
+		
+		return resultList;
+	}
+
+	@Override
+	public String actualizar(RepEntradas e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String guardar(RepEntradas e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String eliminar(RepEntradas e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String eliminarListado(List<RepEntradas> listado) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
---
+++ src/main/java/mx/com/ferbo/ui/RepEntradas.java
@@ -0,0 +1,162 @@
+package mx.com.ferbo.ui;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class RepEntradas {
+	String folioCliente = null;
+	Date fechaIngreso = null;
+	String numeroCliente = null;
+	String nombreCliente = null;
+	String productoDescripcion = null;
+	BigDecimal pesoTotal = null;
+	Integer cantidadTotal = null;
+	BigDecimal valorMercancia = null;
+	String dtpCodigo = null;
+	String dtpSap = null;
+	String dtpLote = null;
+	String dtpPo = null;
+	Date dtpCaducidad = null;
+	String dtpTarimas = null;
+	Integer cantidadUnidadManejo = null;
+	String unidadManejoDescripcion = null;
+	Integer idPlanta = null;
+	String nombrePlanta = null;
+	Integer idCamara = null;
+	String nombreCamara = null;
+	
+	public String getFolioCliente() {
+		return folioCliente;
+	}
+	public void setFolioCliente(String folioCliente) {
+		this.folioCliente = folioCliente;
+	}
+	public Date getFechaIngreso() {
+		return fechaIngreso;
+	}
+	public void setFechaIngreso(Date fechaIngreso) {
+		this.fechaIngreso = fechaIngreso;
+	}
+	public String getNumeroCliente() {
+		return numeroCliente;
+	}
+	public void setNumeroCliente(String numeroCliente) {
+		this.numeroCliente = numeroCliente;
+	}
+	public String getNombreCliente() {
+		return nombreCliente;
+	}
+	public void setNombreCliente(String nombreCliente) {
+		this.nombreCliente = nombreCliente;
+	}
+	public String getProductoDescripcion() {
+		return productoDescripcion;
+	}
+	public void setProductoDescripcion(String productoDescripcion) {
+		this.productoDescripcion = productoDescripcion;
+	}
+	public BigDecimal getPesoTotal() {
+		return pesoTotal;
+	}
+	public void setPesoTotal(BigDecimal pesoTotal) {
+		this.pesoTotal = pesoTotal;
+	}
+	public Integer getCantidadTotal() {
+		return cantidadTotal;
+	}
+	public void setCantidadTotal(Integer cantidadTotal) {
+		this.cantidadTotal = cantidadTotal;
+	}
+	public BigDecimal getValorMercancia() {
+		return valorMercancia;
+	}
+	public void setValorMercancia(BigDecimal valorMercancia) {
+		this.valorMercancia = valorMercancia;
+	}
+	public String getDtpCodigo() {
+		return dtpCodigo;
+	}
+	public void setDtpCodigo(String dtpCodigo) {
+		this.dtpCodigo = dtpCodigo;
+	}
+	public String getDtpSap() {
+		return dtpSap;
+	}
+	public void setDtpSap(String dtpSap) {
+		this.dtpSap = dtpSap;
+	}
+	public String getDtpLote() {
+		return dtpLote;
+	}
+	public void setDtpLote(String dtpLote) {
+		this.dtpLote = dtpLote;
+	}
+	public String getDtpPo() {
+		return dtpPo;
+	}
+	public void setDtpPo(String dtpPo) {
+		this.dtpPo = dtpPo;
+	}
+	public Date getDtpCaducidad() {
+		return dtpCaducidad;
+	}
+	public void setDtpCaducidad(Date dtpCaducidad) {
+		this.dtpCaducidad = dtpCaducidad;
+	}
+	public String getDtpTarimas() {
+		return dtpTarimas;
+	}
+	public void setDtpTarimas(String dtpTarimas) {
+		this.dtpTarimas = dtpTarimas;
+	}
+	public Integer getCantidadUnidadManejo() {
+		return cantidadUnidadManejo;
+	}
+	public void setCantidadUnidadManejo(Integer cantidadUnidadManejo) {
+		this.cantidadUnidadManejo = cantidadUnidadManejo;
+	}
+	public String getUnidadManejoDescripcion() {
+		return unidadManejoDescripcion;
+	}
+	public void setUnidadManejoDescripcion(String unidadManejoDescripcion) {
+		this.unidadManejoDescripcion = unidadManejoDescripcion;
+	}
+	public Integer getIdPlanta() {
+		return idPlanta;
+	}
+	public void setIdPlanta(Integer idPlanta) {
+		this.idPlanta = idPlanta;
+	}
+	public String getNombrePlanta() {
+		return nombrePlanta;
+	}
+	public void setNombrePlanta(String nombrePlanta) {
+		this.nombrePlanta = nombrePlanta;
+	}
+	public Integer getIdCamara() {
+		return idCamara;
+	}
+	public void setIdCamara(Integer idCamara) {
+		this.idCamara = idCamara;
+	}
+	public String getNombreCamara() {
+		return nombreCamara;
+	}
+	public void setNombreCamara(String nombreCamara) {
+		this.nombreCamara = nombreCamara;
+	}
+	@Override
+	public String toString() {
+		return "{\"folioCliente\":\"" + folioCliente + "\", \"fechaIngreso\":\"" + fechaIngreso
+				+ "\", \"numeroCliente\":\"" + numeroCliente + "\", \"nombreCliente\":\"" + nombreCliente
+				+ "\", \"productoDescripcion\":\"" + productoDescripcion + "\", \"pesoTotal\":\"" + pesoTotal
+				+ "\", \"cantidadTotal\":\"" + cantidadTotal + "\", \"valorMercancia\":\"" + valorMercancia
+				+ "\", \"dtpCodigo\":\"" + dtpCodigo + "\", \"dtpSap\":\"" + dtpSap + "\", \"dtpLote\":\"" + dtpLote
+				+ "\", \"dtpPo\":\"" + dtpPo + "\", \"dtpCaducidad\":\"" + dtpCaducidad + "\", \"dtpTarimas\":\""
+				+ dtpTarimas + "\", \"cantidadUnidadManejo\":\"" + cantidadUnidadManejo
+				+ "\", \"unidadManejoDescripcion\":\"" + unidadManejoDescripcion + "\", \"idPlanta\":\"" + idPlanta
+				+ "\", \"nombrePlanta\":\"" + nombrePlanta + "\", \"idCamara\":\"" + idCamara
+				+ "\", \"nombreCamara\":\"" + nombreCamara + "\"}";
+	}
+	
+}
