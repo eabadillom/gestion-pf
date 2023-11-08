c39b5eb434c46dce101c1d412f38abf8f1fef610
Giovanny2999 <monti290399@hotmail.com>
2023-11-08 16:18:26 -0600

    orden Entrada datos generales

--- src/main/java/mx/com/ferbo/controller/OrdenEntradaBean.java
+++ src/main/java/mx/com/ferbo/controller/OrdenEntradaBean.java
@@ -4,8 +4,11 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.application.FacesMessage.Severity;
 import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
@@ -14,10 +17,15 @@ import javax.servlet.http.HttpSession;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.primefaces.PrimeFaces;
+
 import mx.com.ferbo.dao.ClienteDAO;
 import mx.com.ferbo.dao.IngresoDAO;
+import mx.com.ferbo.dao.ProductoClienteDAO;
 import mx.com.ferbo.model.Cliente;
 import mx.com.ferbo.model.Ingreso;
+import mx.com.ferbo.model.Producto;
+import mx.com.ferbo.model.ProductoPorCliente;
 import mx.com.ferbo.model.Usuario;
 import mx.com.ferbo.util.DateUtil;
 
@@ -36,7 +44,18 @@ public class OrdenEntradaBean implements Serializable {
 	private IngresoDAO ingresoDAO;
 	private Ingreso ingreso;
 	
+	private List<ProductoPorCliente> listaProductoPorCte;
+	private ProductoClienteDAO productoClienteDAO;
+	
+	private List<Producto> listaProductos;
+	private Producto producto;
+	
 	private Date fechaActual;
+	
+	FacesContext faceContext = null;
+	HttpServletRequest request = null;
+	HttpSession session = null;
+	Usuario usuario = null;
 	
 	public OrdenEntradaBean(){
 		
@@ -48,6 +67,12 @@ public class OrdenEntradaBean implements Serializable {
 		ingresoDAO = new IngresoDAO();
 		ingreso = new Ingreso();
 		
+		listaProductoPorCte = new ArrayList<ProductoPorCliente>();
+		productoClienteDAO = new ProductoClienteDAO();
+		
+		listaProductos = new ArrayList<Producto>();
+		producto = new Producto();
+				
 		fechaActual = new Date();
 		
 	}
@@ -55,16 +80,29 @@ public class OrdenEntradaBean implements Serializable {
 	@PostConstruct
 	public void init() {
 		
+		faceContext = FacesContext.getCurrentInstance();
+        request = (HttpServletRequest) faceContext.getExternalContext().getRequest();
+        session = request.getSession(false);
+        
+        usuario = (Usuario) session.getAttribute("usuario");
+		
 		listaClientes = clienteDAO.buscarTodos();
 		
 	}
 	
-	public void ordenesEntrada() {
+	public void cargaInfoCliente() {
 		
-		FacesContext faceContext = null;
-		HttpServletRequest request = null;
-		HttpSession session = null;
-		Usuario usuario = null;
+		ordenesEntrada();
+		productoCte();
+		
+	}
+	
+	public void ordenesEntrada() {		
+		
+		FacesMessage message = null;
+		Severity severity = null;
+		String mensaje = null;
+		
 		
 		try {
 			
@@ -72,23 +110,37 @@ public class OrdenEntradaBean implements Serializable {
 			DateUtil.setTime(fechaActualIni, 0, 0, 0, 0);
 			
 			Date fechaActualFin = new Date(fechaActual.getTime());
-			DateUtil.setTime(fechaActualFin, 23, 59, 59, 99);
-			
-			faceContext = FacesContext.getCurrentInstance();
-	        request = (HttpServletRequest) faceContext.getExternalContext().getRequest();
-	        session = request.getSession(false);
-	        
-	        usuario = (Usuario) session.getAttribute("usuario");
+			DateUtil.setTime(fechaActualFin, 23, 59, 59, 99);	        	       
 	        
 	        listaIngreso = ingresoDAO.buscarPorFechaCtePlanta(fechaActualIni, fechaActualFin, cliente.getCteCve(),usuario.getIdPlanta());
 			
+	        severity = FacesMessage.SEVERITY_INFO;
+	        mensaje = "Selecciona Folio de Orden de Entrada";
 		} catch (Exception e) {
 			log.info("Error ...." + e.getMessage());
+			
+			severity = FacesMessage.SEVERITY_INFO;
+	        mensaje = "Error al encontrar ordenes de entrada";
+			
+		}finally {
+			message = new FacesMessage(severity, "Orden Entrada", mensaje);
+			FacesContext.getCurrentInstance().addMessage(null, message);
+			PrimeFaces.current().ajax().update("form:messages");
 		}
 		
 	}
 	
-
+	public void productoCte() {
+		
+		listaProductos.clear();
+		listaProductoPorCte = productoClienteDAO.buscarPorCliente(cliente.getCteCve(), false);
+		
+		for(ProductoPorCliente pc: listaProductoPorCte) {			
+			Producto prod = pc.getProductoCve();
+			listaProductos.add(prod);
+		}
+		
+	}
 	
 	
 	public Cliente getCliente() {
@@ -121,6 +173,30 @@ public class OrdenEntradaBean implements Serializable {
 
 	public void setIngreso(Ingreso ingreso) {
 		this.ingreso = ingreso;
+	}
+
+	public List<ProductoPorCliente> getListaProductoPorCte() {
+		return listaProductoPorCte;
+	}
+
+	public void setListaProductoPorCte(List<ProductoPorCliente> listaProductoPorCte) {
+		this.listaProductoPorCte = listaProductoPorCte;
+	}
+
+	public List<Producto> getListaProductos() {
+		return listaProductos;
+	}
+
+	public void setListaProductos(List<Producto> listaProductos) {
+		this.listaProductos = listaProductos;
+	}
+
+	public Producto getProducto() {
+		return producto;
+	}
+
+	public void setProducto(Producto producto) {
+		this.producto = producto;
 	}
 	
 
