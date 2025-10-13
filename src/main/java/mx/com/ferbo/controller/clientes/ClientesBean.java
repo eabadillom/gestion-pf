f310715802e6ac959da1caad34830d74e61700f6
julio <julio@juliopc.ferbo>
2025-10-13 09:27:18 -0600

    Limpiar código

--- src/main/java/mx/com/ferbo/controller/clientes/ClientesBean.java
+++ src/main/java/mx/com/ferbo/controller/clientes/ClientesBean.java
@@ -10,45 +10,5 @@ import javax.inject.Named;
 @ViewScoped
 public class ClientesBean implements Serializable {
 
-    // Objetos de clientes
-
-    // Objetos para contactos
-
-    // Ejemplo de estrucutura de una funcion
-
-    /*
-     * public tipo numbreFuncion(Parametros si lleva){
-     * try {
-     * addMessage(FacesMessage.SEVERITY_INFO, "Servicio", "Servicio Eliminado");
-     * } catch (IneventatioException ex) {
-     * addMessage(FacesMessage.SEVERITY_WARN, "Servicio", ex.getMessage());
-     * } catch (exception){
-     * addMessage(FacesMessage.SEVERITY_ERROR, "Servicio",
-     * "Contacte con el admistrador del sistema.");
-     * } finally {
-     * PrimeFaces.current().ajax().update("form:messages",
-     * "form:tabView:dt-servicios");
-     * }
-     * }
-     * 
-     * private void addMessage(FacesMessage.Severity severity, String title, String
-     * msg) {
-     * FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(severity,
-     * title, msg));
-     * }
-     * 
-     */
-
-    // Metodos exclusivos de servicios
-
-    // Metodos exclusivos de domicilios
-
-    // Metodos exclusivos para contactos
-
-    // Getter y Setter para Cliente
-
-    // Getters y Setters para contactos
-
-    // Getter y Setter de sidebar
 
 }
